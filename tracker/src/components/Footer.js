6f1ae2b4bc6a4ce24c945a04c4c9a3dd66446345
davemolk <davemolk@gmail.com>
2021-09-02 10:04:48 -0600

    json-server implemented

---
+++ tracker/src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>Dave Molk &copy; 2021</p>
+            <a href="/about">About</a>
+        </footer>
+    )
+}
+
+export default Footer
--- tracker/src/components/Header.js
+++ tracker/src/components/Header.js
@@ -1,10 +1,18 @@
 import Button from './Button'
+import { useLocation } from 'react-router-dom'
 
 const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
+
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color={showAdd ? 'grey': 'blue'} text={showAdd ? 'Close': 'Add'} onClick={onAdd}/>
+            {location.pathname === '/' && (
+                <Button 
+                    color={showAdd ? 'grey': 'blue'} 
+                    text={showAdd ? 'Close': 'Add'} 
+                    onClick={onAdd}/>
+            )}
         </header>
     )
 }
